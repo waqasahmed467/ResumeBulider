60444c1322e5b738944164e27d15031bc53d53bf
waqasAhmed223 <waqasahmedkhan1243@gmail.com>
2025-12-10 11:24:33 -0800

    aha

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Components/header'
+const App = () => {
+  return (
+    <div className='bg-pink-200 w-full h-screen'>
+      <Header/>
+      app
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const header = () => {
+  return (
+    <div className='w-full text-white flex items-center p-10 bg-red-500'>
+      <h1 >Resume Bulider acha g esa he kai</h1>
+      
+    </div>
+  )
+}
+
+export default header
